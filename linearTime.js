161be0b1d1b8cf1ee658433a86aea7f94478c895
SURAJ PATIL <37582161+SURAJKP@users.noreply.github.com>
2021-08-09 11:23:59 +0000

    Linear Time complexity O(n)

---
+++ linearTime.js
@@ -0,0 +1,11 @@
+const arr =new Array(1000000).fill("Hello");
+
+findHello(arr) {
+  for(let i=0; i<rr.length; i++) {
+    if(arr[i] === "Hello") {
+      console.log("Found Hello");
+    }
+  }
+}
+
+findHello(arr); // Linear Time O(n)

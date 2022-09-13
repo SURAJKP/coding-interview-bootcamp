d65f35a04e0a72f523551747bee059757177cabf
SURAJ PATIL <37582161+SURAJKP@users.noreply.github.com>
2022-09-13 12:54:19 +0000

    updated

--- linearTime.js
+++ linearTime.js
@@ -1,15 +1,15 @@
-const arr = new Array(1000000).fill("Hello");
+const arr = new Array(1000000).fill('Hello');
 
-findHello = (arr) => {
-  const t0 = performance.now();
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === "Hello") {
-      console.log("Found Hello");
-      break; // more efficient code
-    }
-  }
-  const t1 = performance.now();
-  console.log('findHello call took' + (t1 - t0) + 'millisecondes');
-}
+findHello = arr => {
+	const t0 = performance.now();
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === 'Hello') {
+			console.log('Found Hello');
+			break; // more efficient code
+		}
+	}
+	const t1 = performance.now();
+	console.log('findHello call took' + (t1 - t0) + 'millisecondes');
+};
 
 findHello(arr); // Linear Time O(n)
